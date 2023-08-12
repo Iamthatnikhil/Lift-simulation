82d114c46417022f634f12a9756b0a9a66a59132
Iamthatnikhil <nikhilasssudani@gmail.com>
2023-08-12 16:01:55 +0530

    init

---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementById('inputForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const container = document.querySelector(".container")
+    const floorValue = parseInt(document.getElementById('floor').value);
+    const liftValue = parseInt(document.getElementById('lift').value);
+  
+    // Check if both values are valid (greater than 0)
+    if (floorValue > 0 && liftValue > 0) {
+      // Clear previous divs, if any
+      document.getElementById('container').innerHTML = '';
+      document.getElementById('container').style.display = 'none';
+      
+    
+  
+      // Generate the floor divs
+      for (let i = 1; i <= floorValue; i++) {
+        const newDiv = document.createElement('div');
+        newDiv.textContent = `Floor ${i}`;
+        newDiv.classList.add("floor-container");
+        document.getElementById('container').appendChild(newDiv);
+
+        const upButton = document.createElement('button');
+      upButton.textContent = 'Up';
+
+      const downButton = document.createElement('button');
+      downButton.textContent = 'Down';
+
+      newDiv.appendChild(upButton);
+      newDiv.appendChild(downButton);
+
+
+      }
+  
+      // Generate the lift divs
+      for (let i = 1; i <= liftValue; i++) {
+        const newDiv = document.createElement('div');
+        newDiv.textContent = `Lift ${i}`;
+        newDiv.classList.add("lift");
+        document.getElementById('container').appendChild(newDiv);
+      }
+    } else {
+      alert('Please enter valid values. Floor and Lift values should be greater than 0.');
+    }
+  });
